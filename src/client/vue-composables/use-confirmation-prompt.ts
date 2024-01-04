205530ec1929ff6b99b7c03e24394118464e3586
evgeniy <fxzhukov@gmail.com>
2024-01-04 14:59:49 +0100

    fix: simplify logic

--- src/client/vue-composables/use-confirmation-prompt.ts
+++ src/client/vue-composables/use-confirmation-prompt.ts
@@ -1,7 +1,6 @@
 import { Ref, ref, DefineComponent, defineComponent, createApp } from 'vue'
 
 let confirmationModalComponent: DefineComponent<unknown, unknown, unknown> | null = null
-const isMounted = false
 
 export type SetupOptions = {
   message: Ref<string>
@@ -56,55 +55,62 @@ const useConfirmationPrompt = <Entity extends unknown>(options: Options<Entity>)
   if (!selectedEntity) {
     selectedEntity = ref(null)
   }
-  if (!isMounted) {
-    const modalView = defineComponent({
-      extends: confirmationModalComponent,
-      data() {
-        if (!setupOptions) {
-          throw new Error('Setup options should be present here')
-        }
-        return {
-          ...setupOptions,
-          onCancel: () => {
-            confirmCallbacks = []
-            resetOptions()
-          },
-          onConfirm: async () => {
-            if (!setupOptions) return
-            const currentEntity = selectedEntity?.value ?? null
-            setupOptions.processing.value = true
-            try {
-              await Promise.all(confirmCallbacks.map((callback) => callback(currentEntity)))
-            } catch {
-              //
-            }
-            resetOptions()
-            // @ts-expect-error entity type is lost here
-            options.onConfirmed(currentEntity)
+
+  const modalView = defineComponent({
+    extends: confirmationModalComponent,
+    data() {
+      if (!setupOptions) {
+        throw new Error('Setup options should be present here')
+      }
+      return {
+        ...setupOptions,
+        onCancel: () => {
+          confirmCallbacks = []
+          resetOptions()
+        },
+        onConfirm: async () => {
+          if (!setupOptions) return
+          const currentEntity = selectedEntity?.value ?? null
+          setupOptions.processing.value = true
+          try {
+            await Promise.all(confirmCallbacks.map((callback) => callback(currentEntity)))
+          } catch {
+            //
           }
+          resetOptions()
+          // @ts-expect-error entity type is lost here
+          options.onConfirmed(currentEntity)
         }
       }
-    })
-    const div = document.createElement('div')
-    const element = document.getElementById('app')
-    if (!element) {
-      throw new Error('There is no `#app` html element')
     }
-    element.appendChild(div)
-    createApp(modalView).mount(div)
+  })
+
+  const div = document.createElement('div')
+  const element = document.getElementById('app')
+
+  if (!element) {
+    throw new Error('There is no `#app` html element')
   }
+
+  element.appendChild(div)
+  createApp(modalView).mount(div)
+
   const trigger = (entity: Entity) => {
     if (!setupOptions || !selectedEntity) {
       throw new Error('Setup options or entity ref are not present')
     }
-    if (setupOptions.show.value || selectedEntity.value) return
+
+    if (setupOptions.show.value) return
+
     setupOptions.message.value =
       typeof options.question === 'function' ? options.question(entity) : options.question ?? ''
+
     setupOptions.cancelText.value = options.cancelText ?? 'Cancel'
     setupOptions.confirmText.value = options.confirmText ?? 'OK'
     selectedEntity.value = entity
     setupOptions.show.value = true
     setupOptions.processing.value = false
+
     const callback = options.onConfirmed as (ent: unknown) => Promise<void>
     confirmCallbacks.push(callback)
   }
